2ab828af935f4989ea6ea6656f178a50eea378e9
Anderson <anderson.medeiros3011@gmail.com>
2026-06-04 18:09:47 -0300

    Herbivores and carnivores3'

--- app/main.py
+++ app/main.py
@@ -3,10 +3,11 @@ class Animal:
 
     def __init__(
             self,
-            name: str
+            name: str,
+            health: int = 100
     ) -> None:
         self.name = name
-        self.health = 100
+        self.health = health
         self.hidden = False
         Animal.alive.append(self)
 
